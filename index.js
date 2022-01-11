8b10a825ad2ed580cb59d0d7fd3b1c5f6353dd38
Jack Pena <penajackc@gmail.com>
2022-01-11 16:43:35 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, str){
+    return arr.filter(element => element.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(arr, str){
+    return arr.filter(element => element.startsWith(str));
+}
+
+function matchName(arr, str){
+    return arr.filter(element => element.name === str)
+}
